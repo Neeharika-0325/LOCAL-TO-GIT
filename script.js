7740fac7c241c6512a91c9cc367fb7e2f7b6f501
Neeharika-0325 <neeharikamarella3@gmail.com>
2023-02-20 17:11:39 +0530

    added simple button functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+let addedtext=document.getElementById("text");
+        if(addEventListener(getElementById("btn"), onclick)){
+            addedtext+= document.getElementById("text").innerHTML;
+        }
+        
